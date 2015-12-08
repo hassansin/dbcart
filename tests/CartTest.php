d107773c60fcda37b46b148172f7800363808d88
hassansin <mhkr63@gmail.com>
2015-12-08 06:50:51 +0600

    added testsuite

--- tests/CartTest.php
+++ tests/CartTest.php
@@ -1,18 +1,44 @@
 <?php
 
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Hassansin\DBCart\Models\Cart;
+use Hassansin\DBCart\Models\CartLine;
 
-class CartTest extends TestCase
+class CartTest extends Orchestra\Testbench\TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
+
+    public function setUp()
     {
-        $this->assertTrue(true);
+        parent::setUp();
+        $this->artisan('migrate', [
+            '--database' => 'testbench',
+            '--realpath' => realpath(__DIR__.'/../src/Hassansin/DBCart/database/migrations'),
+        ]);
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [Hassansin\DBCart\CartServiceProvider::class];
+    }
+
+    protected function getEnvironmentSetUp($app)
+    {
+        // Setup default database to use sqlite :memory:
+        $app['config']->set('database.default', 'testbench');
+        $app['config']->set('database.connections.testbench', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
+    }
+    
+    public function testCreateItem()
+    {
+        $cart = Cart::create();
+        $cart->addItem([
+            'product_id' => 1,
+            'unit_price' => 10.5,
+            'quantity' => 1
+        ]); 
+        $this->assertCount(1, $cart->items);
     }
 }
